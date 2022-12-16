0b72149626ac9604b5fd95f6d63a3f696fd45bc6
Megapixel-code <77630002+Megapixel-code@users.noreply.github.com>
2022-12-16 10:00:11 +0100

    Update shell.sh

--- shell.sh
+++ shell.sh
@@ -1,1 +1,29 @@
+#!/bin/bash
 
+totarg=$#
+
+#precipitation temperature humidite vitesse orientation_du_vent periode 
+#prec temp humi vite orie 
+
+if [ $totarg -lt 4 ]
+then
+    echo "probleme il faut 4 arg minimum avec:
+1-station 2-date_start 3-date_stop 
+
+4->8 :
+pression vent humidite precipitation temperature"
+    exit 1
+fi
+
+arg1=$1
+arg2=$2
+arg3=$3
+arg4=$4
+arg5=$5
+
+#for VAR in 
+
+echo $arg5
+
+
+echo $totarg
