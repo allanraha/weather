c5dff4472ec15b4d6f8b365abb05dff970599104
Megapixel-code <77630002+Megapixel-code@users.noreply.github.com>
2023-02-01 22:46:49 +0100

    Update main.c

--- main.c
+++ main.c
@@ -1,29 +1,56 @@
-#include <stdlib.h>
+#include <ctype.h>
 #include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
 
+//  ./main -f <file> -o <output_file> -r (optional, reverse) --tab/--abr/--avl (optional)
 
-int get_args(){
-    FILE * file;
-    char ch;
-    
-    file = fopen("temp.txt", "r");
+int main(int argc, char **argv){
+    int fflag = 0;
+    int oflag = 0;
+    int rflag = 0;
+    int optflag = 0; 
+    int c;
 
-    if (file == NULL){
-        printf("erreur ouverture temp\n");
-        return 1;
+    for (int i = 0; i<argc; i++){
+        if (strcmp(argv[i], "--tab")){
+            argv[i] = "-0";
+        }
+        else if (strcmp(argv[i], "--abr")){
+            argv[i] = "-1";
+        }
+        else if (strcmp(argv[i], "--avl")){
+            argv[i] = "-2";
+        }
     }
 
-    do {
-        ch = fgetc(file);
-        printf("%c", ch);
-    }while (ch != EOF);
+    while ((c = getopt(argc, argv, "f:o:r012")) != -1) switch (c){
+        case 'f':
+            fflag = 1;
+            break;
+        case 'o':
+            oflag = 1;
+            break;
+        case 'r':
+            rflag = 1;
+            break;
+        case '0':
+            optflag = 1;
+            break;
 
-    fclose(file);
-    return 0;
-}
 
+        case '?':
+            fprintf(stderr, "Option `-%c' non existante\n", optopt);
+            return 1;
+        default:
+            abort();
+        }
 
-int main(){
-    get_args();
+    if (!fflag || !oflag){
+        return 1;
+    }
+    
+    printf("fflag = %d, oflag = %d\n", fflag, oflag);
+
     return 0;
 }
