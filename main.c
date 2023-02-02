9f9bab88e2a08d5ad6bb1f82c8b349a61e523a7e
Megapixel-code <77630002+Megapixel-code@users.noreply.github.com>
2023-02-02 16:11:06 +0100

    Update main.c

--- main.c
+++ main.c
@@ -1,4 +1,5 @@
 #include <ctype.h>
+#include <string.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
@@ -9,48 +10,64 @@ int main(int argc, char **argv){
     int fflag = 0;
     int oflag = 0;
     int rflag = 0;
-    int optflag = 0; 
-    int c;
-
+    int optflag = 2; //tab = 0 abr = 1 avl = 2
+    int c = 0;
+    
     for (int i = 0; i<argc; i++){
-        if (strcmp(argv[i], "--tab")){
-            argv[i] = "-0";
+        if (strcmp(argv[i], "--tab") == 0){
+            argv[i] = "-a";
         }
-        else if (strcmp(argv[i], "--abr")){
-            argv[i] = "-1";
+        else if (strcmp(argv[i], "--abr") == 0){
+            argv[i] = "-b";
         }
-        else if (strcmp(argv[i], "--avl")){
-            argv[i] = "-2";
+        else if (strcmp(argv[i], "--avl") == 0){
+            argv[i] = "-c";
         }
     }
 
-    while ((c = getopt(argc, argv, "f:o:r012")) != -1) switch (c){
+    char *input_file = NULL;
+    char *output_file = NULL;
+
+    while ((c = getopt(argc, argv, "f:o:rabc")) != -1)
+        switch (c){
         case 'f':
             fflag = 1;
+            input_file = optarg;
             break;
         case 'o':
             oflag = 1;
+            output_file = optarg;
             break;
         case 'r':
             rflag = 1;
             break;
-        case '0':
+        case 'a':
+            printf("non");
+            optflag = 0;
+            break;
+        case 'b':
+            printf("oui");
             optflag = 1;
             break;
-
+        case 'c':
+            optflag = 2;
+            break;
 
         case '?':
             fprintf(stderr, "Option `-%c' non existante\n", optopt);
             return 1;
         default:
+            printf("not recognized\n");
             abort();
-        }
-
-    if (!fflag || !oflag){
-        return 1;
     }
     
-    printf("fflag = %d, oflag = %d\n", fflag, oflag);
+    if (!(fflag & oflag)){
+        return 1;
+    }
+
+    printf("\n\n");
+    printf("fflag = %d, oflag = %d, rflag = %d, optflag = %d\n", fflag, oflag, rflag, optflag);
+    printf("input_file = %s, output_file = %s\n", input_file, output_file);
 
     return 0;
 }
