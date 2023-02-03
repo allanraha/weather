61c65d482c1ec8384ad387308504281138fcca89
Megapixel-code <77630002+Megapixel-code@users.noreply.github.com>
2023-02-03 17:47:18 +0100

    Update main.c

--- main.c
+++ main.c
@@ -4,27 +4,13 @@
 #include <stdlib.h>
 #include <unistd.h>
 
-typedef struct Arbre{
+typedef struct Node{
 	int elt;
-	struct Arbre * fg;
-	struct Arbre * fd;
+	char *str;
+	struct Node * fg;
+	struct Node * fd;
 	int equilibre;	
-}Arbre;
-
-Arbre * creerArbre(int e){
-	Arbre * noeud = malloc(sizeof(Arbre));
-	if(noeud==NULL){
-		printf("Impossible de creer l'arbre");
-		exit(1);
-	}
-	else{
-		noeud->elt = e;
-		noeud->fg = NULL;
-		noeud->fd = NULL;
-		noeud->equilibre = 0;
-	}
-	return noeud;
-}
+}Node;
 
 int max(int a,int b){
 	if(a>b){
@@ -44,8 +30,25 @@ int min(int a,int b){
 	}
 }
 
-Arbre * rotationGauche(Arbre * a){
-	Arbre * pivot;
+Node * creerNode(int e, char *str_val){
+	printf("test");
+	Node * noeud = malloc(sizeof(Node));
+	if(noeud==NULL){
+		printf("Impossible de creer l'Node");
+		exit(1);
+	}
+	else{
+		noeud->elt = e;
+		noeud->str = str_val;
+		noeud->fg = NULL;
+		noeud->fd = NULL;
+		noeud->equilibre = 0;
+	}
+	return noeud;
+}
+
+Node * rotationGauche(Node * a){
+	Node * pivot;
 	int eq_a;
 	int eq_p;
 	
@@ -61,8 +64,8 @@ Arbre * rotationGauche(Arbre * a){
 		
 }
 
-Arbre * rotationDroite(Arbre * a){
-	Arbre * pivot;
+Node * rotationDroite(Node * a){
+	Node * pivot;
 	int eq_a;
 	int eq_p;
 	
@@ -78,17 +81,17 @@ Arbre * rotationDroite(Arbre * a){
 		
 }
 
-Arbre * doubleRotationGauche(Arbre * a){
+Node * doubleRotationGauche(Node * a){
 	a->fd = rotationDroite(a->fd);
 	return(rotationGauche(a));
 }
 
-Arbre * doubleRotationDroite(Arbre * a){
+Node * doubleRotationDroite(Node * a){
 	a->fg = rotationGauche(a->fg);
 	return(rotationDroite(a));
 }
 
-Arbre * equilibreAVL(Arbre * a){
+Node * equilibreAVL(Node * a){
 	if(a->equilibre == 2){
 		if(a->fd->equilibre >= 0){
 			return(rotationGauche(a));
@@ -108,17 +111,17 @@ Arbre * equilibreAVL(Arbre * a){
 	return(a);
 }
 
-Arbre * insertionAVL(Arbre * a,int e,int * h){
+Node * insertionAVL(Node * a,int e,int * h, char *str_val){
 	if(a == NULL){
 		*h=1;
-		return(creerArbre(e));
+		return(creerNode(e, str_val));
 	}
 	if(e < a->elt){
-		a->fg = insertionAVL(a->fg,e,h);
+		a->fg = insertionAVL(a->fg,e,h,str_val);
 		*h = -*h;
 	}
 	else if( e > a->elt){
-		a->fd = insertionAVL(a->fd,e,h);
+		a->fd = insertionAVL(a->fd,e,h,str_val);
 	}
 	else{
 		*h=0;
@@ -138,33 +141,71 @@ Arbre * insertionAVL(Arbre * a,int e,int * h){
 	return(a);
 }
 
+FILE * fetch_file(char *file_name){
+	FILE *f = NULL;
+	f = fopen(file_name, "r");
+	if (f == NULL){
+		exit(2);
+	}
+	return f;
+}
 
-//  ./main -f <file> -o <output_file> -r (optional, reverse) --tab/--abr/--avl (optional)
+void parcours_save(FILE *file, Node *arbre){
+	fprintf(file, "%s", arbre->str);
+}
+
+FILE * sort(FILE *file, int column, FILE *output_file){
+	char str[200];
+	char strcp[200];
+	char *temp;
+	int val;
+	int i;
+
+	Node *arbre = NULL;
+
+	while (fgets(str, 200, file)!=NULL){
+		strcpy(strcp, str);
+		temp = strtok(strcp, ";");
+		for (i = 0; i < column; i++){
+			temp = strtok(NULL, ";");
+		}
+		val = atoi(temp);
+		
+		arbre = insertionAVL(arbre, val, 0, str);
+	}
+
+	//parcours_save(output_file, arbre);
+}
+
+
+
+//  ./main -f <file> -o <output_file> -c <column_sorted> -r (optional, reverse) --tab/--abr/--avl (optional)
 
 int main(int argc, char **argv){
     int fflag = 0;
     int oflag = 0;
     int rflag = 0;
     int optflag = 2; //tab = 0 abr = 1 avl = 2
-    int c = 0;
+    int _arg = 0;
     
     for (int i = 0; i<argc; i++){
         if (strcmp(argv[i], "--tab") == 0){
-            argv[i] = "-a";
+            argv[i] = "-x";
         }
         else if (strcmp(argv[i], "--abr") == 0){
-            argv[i] = "-b";
+            argv[i] = "-y";
         }
         else if (strcmp(argv[i], "--avl") == 0){
-            argv[i] = "-c";
+            argv[i] = "-z";
         }
     }
 
     char *input_file = NULL;
     char *output_file = NULL;
+	int column_to_sort;
 
-    while ((c = getopt(argc, argv, "f:o:rabc")) != -1)
-        switch (c){
+    while ((_arg = getopt(argc, argv, "f:o:rxyzc:")) != -1)
+        switch (_arg){
         case 'f':
             fflag = 1;
             input_file = optarg;
@@ -176,17 +217,19 @@ int main(int argc, char **argv){
         case 'r':
             rflag = 1;
             break;
-        case 'a':
-            printf("non");
+        case 'x':
             optflag = 0;
             break;
-        case 'b':
-            printf("oui");
+        case 'y':
             optflag = 1;
             break;
-        case 'c':
+        case 'z':
             optflag = 2;
             break;
+		case 'c':
+
+			column_to_sort = atoi(optarg);
+			break;
 
         case '?':
             fprintf(stderr, "Option `-%c' non existante\n", optopt);
@@ -200,9 +243,18 @@ int main(int argc, char **argv){
         return 1;
     }
 
-    printf("\n\n");
-    printf("fflag = %d, oflag = %d, rflag = %d, optflag = %d\n", fflag, oflag, rflag, optflag);
-    printf("input_file = %s, output_file = %s\n", input_file, output_file);
+	FILE *data;
+	data = fetch_file(input_file);
+
+	FILE *foutput;
+	foutput = fopen(output_file, "w");
+	if (foutput == NULL){
+		exit(2);
+	}
+
+	sort(data, 1, foutput);
+
+	fclose(data);
 
     return 0;
 }
